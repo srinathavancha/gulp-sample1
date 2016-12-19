6a74d37458fdf46741ccb928d0b4fd689811291b
Avancha Srinath <Avancha Srinath>
2016-12-19 21:31:44 +0530

    second commit

---
+++ app/js/app_1.js
@@ -0,0 +1,3 @@
+console.log('app_1');
+console.log('app_more');
+console.log('app_more more');
---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+// ////////////////////////////////////////
+// Required
+// ////////////////////////////////////////
+var gulp = require('gulp'),
+	uglify = require('gulp-uglify'),
+	uglifyCss = require('gulp-uglifycss'),
+	concat = require('gulp-concat'),
+	rename = require('gulp-rename'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync');
+
+
+// ////////////////////////////////////////
+// Browser Sync
+// ////////////////////////////////////////
+gulp.task('browserSync', function(){
+	browserSync.init({
+		server: ["app", "dist"]
+	});
+});
+
+// ////////////////////////////////////////
+// Scripts Task
+// ////////////////////////////////////////
+gulp.task('scripts', function(){
+	gulp.src(['app/js/**/*.js', '!app/js/**/*.min.js'])
+		.pipe(concat({ path: 'main.min.js', stat: { mode: 0777 }}, {newLine: ';'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('./dist/js'))
+		.pipe(browserSync.reload({stream: true}));
+
+});
+
+// ////////////////////////////////////////
+// Sass Task
+// ////////////////////////////////////////
+gulp.task('sass', function(){
+	return gulp.src('app/scss/style.scss')
+				.pipe(sass())
+				.pipe(gulp.dest('./dist/css'))
+				.pipe(browserSync.reload({stream: true}));
+});
+
+// ////////////////////////////////////////
+// Watch Task
+// ////////////////////////////////////////
+gulp.task('watch', ['browserSync', 'sass'], function(){
+	gulp.watch('app/scss/style.scss', ['sass']);
+	gulp.watch('app/js/**/*.js', ['scripts']);
+	gulp.watch('app/index.html', browserSync.reload);
+});
+// ////////////////////////////////////////
+// Default Task
+// ////////////////////////////////////////
+gulp.task('default', ['scripts', 'watch']);
